687fbb39fb13c480e72186e08ddca3277caa394c
José Ramón Fernández <josferde5@alum.us.es>
2021-08-21 22:11:09 +0200

    Añadidos valores por defecto en 'operation_path' y 'api_specification'

--- IDLChocoWebApp/forms.py
+++ IDLChocoWebApp/forms.py
@@ -14,11 +14,13 @@ class IDLForm(forms.Form):
                       ('isValidRequest', 'IsValidRequest'), ('isValidPartialRequest', 'IsValidPartialRequest')]
     specification_type = forms.ChoiceField(choices=specification_types, label='Specification Type',
                                            initial=('oas', 'Open Api'))
-    operation_path = forms.CharField(required=True, label='Operation Path', validators=[my_validator], widget=forms.TextInput(attrs={'placeholder': '/operation/path'}))
+    operation_path = forms.CharField(required=True, label='Operation Path', validators=[my_validator], widget=forms.TextInput(attrs={'placeholder': '/operation/path'}),
+                                     initial="/search")
     operation_type = forms.ChoiceField(choices=operation_types, label='Operation Types', initial=('GET', 'get'))
     analysis_operation = forms.ChoiceField(choices=analysis_types, label='Analysis Operation',
                                            initial=('isConsistent', 'IsConsistent'))
     request = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'p1=value1&p2=value2...'}), required=False, label='Request to analyze')
     parameter = forms.CharField(required=False, label='Parameter to analyze')
     api_specification = forms.CharField(required=True, widget=forms.Textarea(attrs={'rows': 15}),
-                                        label='API Specification (YAML/JSON or URL to YAML/JSON)')
+                                        label='API Specification (YAML/JSON or URL to YAML/JSON)',
+                                        initial="https://raw.githubusercontent.com/josferde5/IDLReasoner-choco/master/src/test/resources/youtube_search.yaml")
